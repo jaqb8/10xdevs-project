5878ec2ccaff60c49783590de6040fe146839b3f
jaqb8 <jakubaniszewski@pm.me>
2025-12-19 14:42:01 +0100

    refactor: remove console log from AuthInitializer component
    - Eliminated the console log statement in the AuthInitializer to clean up the code and reduce unnecessary logging.

--- src/components/shared/AuthInitializer.tsx
+++ src/components/shared/AuthInitializer.tsx
@@ -10,7 +10,6 @@ export function AuthInitializer({ user }: AuthInitializerProps) {
   const setUser = useAuthStore((state) => state.setUser);
 
   useEffect(() => {
-    console.log("AuthInitializer", user);
     setUser(user);
   }, [user, setUser]);
 
